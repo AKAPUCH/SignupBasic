be9c62f0103acef2cb6235133e699208264f4fb5
Beemo97 <chldnxo5@naver.com>
2022-11-23 21:20:06 +0900

    Initial Commit

---
+++ SignupBasic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignupBasic
+//
+//  Created by 최우태 on 2022/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
