0ab1ffdab5af5cefe071c2200ba1549323c302de
Beemo97 <chldnxo5@naver.com>
2022-11-23 22:56:19 +0900

    1일차 화면1 기능 구현

---
+++ SignupBasic/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  SignupBasic
+//
+//  Created by 최우태 on 2022/11/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SignupBasic/ViewController.swift
+++ SignupBasic/ViewController.swift
@@ -7,8 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var LoginId: UITextField! //초기상태에서는 뷰가 로딩되지 않았기 때문에 변경이 가능하도록 변수(var)로 선언
+    
+    @IBOutlet weak var LoginPw: UITextField!
+    
+    
+    
+    @IBAction func DoSignUp(_ sender : UIButton) {
+        guard let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") else {return}
+        
+        self.present(secondViewController, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
